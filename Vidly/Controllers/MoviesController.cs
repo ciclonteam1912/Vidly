021b85e2a2dec49baf0965895413aaf3276506b0
Juan Moris <moris_juan@hotmail.com>
2017-06-22 21:39:43 -0400

    Formas de pasar datos desde el controlador a la vista.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,7 +13,8 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!"};
-
+            ViewData["Movie"] = movie;
+            ViewBag.Movie = movie;
             return View(movie);
         }
 
