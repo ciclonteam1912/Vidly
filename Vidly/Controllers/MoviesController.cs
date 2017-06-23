0b7612856129775c99c62e1bfa0c44eb2728dbc2
Juan Moris <moris_juan@hotmail.com>
2017-06-22 21:19:24 -0400

    Rutas convencionales.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -17,20 +17,9 @@ namespace Vidly.Controllers
             return View(movie);
         }
 
-        public ActionResult Edit(int id)
+        public ActionResult ByReleaseDate(int year, int month)
         {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (string.IsNullOrEmpty(sortBy))
-                sortBy = "Name";
-
-            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            return Content(year + "/" + month);
         }
     }
 }
