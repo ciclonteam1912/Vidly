f1e3e34a45b075ce80499c9429aeebcf26241ed7
Juan Moris <moris_juan@hotmail.com>
2017-07-05 12:56:16 -0400

    Applying Auto Mapper.

--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -6,6 +6,8 @@ namespace Vidly.Dtos
 {
     public class CustomerDto
     {
+        public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
