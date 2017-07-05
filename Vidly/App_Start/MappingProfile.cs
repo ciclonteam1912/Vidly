a34a05d1d84fff1333fc00643580bc62132cc177
Juan Moris <moris_juan@hotmail.com>
2017-07-05 17:34:06 -0400

    Resolving Exception for updating Id.

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -14,9 +14,13 @@ namespace Vidly.App_Start
         {
             Mapper.CreateMap<CustomerDto, Customer>();
             Mapper.CreateMap<Customer, CustomerDto>();
+            CreateMap<Customer, CustomerDto>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
 
             Mapper.CreateMap<MovieDto, Movie>();
             Mapper.CreateMap<Movie, MovieDto>();
+            CreateMap<Movie, MovieDto>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
 }
